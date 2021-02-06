d33f43f695731f4c253ee3ec6dc90f8354890f19
Andaleeb Khogaly <andaleebkhog@gmail.com>
2021-02-06 23:59:23 +0200

    HTML added, made button works and showed result.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pw';
+  password = '';
+  //Event-Binding-Syntax
+  onButtonClick(){
+    //console.log("button is clicked!");
+    this.password = "This is my password!";
+  }
+  //Property-Binding-Syntax
+  getPassword(){
+    return this.password;
+    //we can use this method instead of property inside the value of input
+  }
 }
